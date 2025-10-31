3586fd3167908660ca7c50610c2f08b6a3d89403
nigilen <you@example.com>
2025-10-31 11:56:46 +0300

    [fix]: replaced useFetch with useAsyncData

--- app/components/BlogGrid.vue
+++ app/components/BlogGrid.vue
@@ -1,8 +1,47 @@
 <script lang="ts" setup>
 
-const { data: posts } = await useFetch('https://cms.nigilen.site/wp-json/wp/v2/posts?_embed');
+interface WPFeaturedMedia {
+  source_url: string;
+};
 
-console.log(posts)
+interface WPPostRaw {
+  id: number;
+  date: string;
+  slug: string;
+  title: { rendered: string };
+  excerpt: { rendered: string };
+  content: { rendered: string };
+  _embedded?: { 'wp:featuredmedia'?: WPFeaturedMedia[] };
+};
+
+interface Post {
+  id: number;
+  dateOnly: string | undefined;
+  slug: string;
+  title: string;
+  excerpt: string;
+  featuredImageUrl: string | null;
+};
+
+
+const { data: posts } = useAsyncData('blog-posts', async (): Promise<Post[]> => {
+  const rawPosts = await $fetch<WPPostRaw[]>(
+    'https://cms.nigilen.site/wp-json/wp/v2/posts',
+    {
+      params: { _embed: 1 }
+    }
+  );
+
+  return rawPosts.map(post => ({
+    id: post.id,
+    dateOnly: post.date.split('T')[0],
+    slug: post.slug,
+    title: post.title.rendered,
+    excerpt: post.excerpt.rendered,
+    featuredImageUrl: post._embedded?.['wp:featuredmedia']?.[0]?.source_url || null
+  }))
+
+});
 
 </script>
 
@@ -12,10 +51,11 @@ console.log(posts)
     <ul class="blog-grid__list">
       <li v-for="post in posts" :key="post" class="blog-grid__item">
         <BlogItem 
-          :title="post.title.rendered"
-          :description="post.excerpt.rendered"
-          :image="post?._embedded?.['wp:featuredmedia']?.[0]?.source_url"
+          :title="post.title"
+          :description="post.excerpt"
+          :image="post.featuredImageUrl"
           :url="`/blog/${post.slug}`"
+          :date="post.dateOnly"
         />
       </li>
     </ul>
